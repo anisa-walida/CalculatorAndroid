bf6f9a6633cb7d73975b48ea06b79fdfa3284603
anisa_walida <anisawalida206@gmail.com>
2023-03-23 00:56:45 +0600

    Updated1

---
+++ app/src/main/java/myCalculator.java
@@ -0,0 +1,26 @@
+public class myCalculator {
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public myCalculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    int a,b;
+
+}
